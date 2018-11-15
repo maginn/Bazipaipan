c015de74599a240306e8cd5b4c2c6168b4b2e346
maginn <44774924+maginn@users.noreply.github.com>
2018-11-15 09:57:19 +0800

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,12 @@
+import BzBaseClass
+
+from BZPaipan import  BzPaipan
+#paipanbySolar(1977,12,7,3)
+
+paipan=BzPaipan()
+
+bzyj=paipan.getPaipanbySun(2002,12,25,8)
+bzyj.getPaiPan()
+
+bzyj=paipan.getPaipanbyLunar(2002,11,22,8)
+bzyj.getPaiPan();
